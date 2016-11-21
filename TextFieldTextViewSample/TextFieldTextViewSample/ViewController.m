25a344c62bb3bd53706114ef3d98f102b991ee93
xu.junyang <xu.junyang@bestv.com.cn>
2016-11-21 21:27:08 +0800

    test45

--- TextFieldTextViewSample/TextFieldTextViewSample/ViewController.m
+++ TextFieldTextViewSample/TextFieldTextViewSample/ViewController.m
@@ -14,6 +14,10 @@
 
 @implementation ViewController
 
+- (void) loadView{
+    [super loadView];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
